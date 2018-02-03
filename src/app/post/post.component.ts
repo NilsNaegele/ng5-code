99f0f21663612626a8a2793d71e512c2beafc74a
NilsHolger <nils.holger.naegele@gmail.com>
2018-02-03 22:09:23 +0100

    template syntax

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -232,41 +232,426 @@ propertyBinding = {
       <app-hero-detail [hero]="currentHero"></app-hero-detail>
       <app-hero-detail prefix="You are my" [hero]="currentHero"></app-hero-detail>
 
-    // ABC.XYZ 123
+      <p><img src="{{ heroImageUrl }}"> is the <i>interpolated</i> image.</p>
+      <p><img [src]="heroImageUrl"> is the <i>property bound</i> image.</p>
+
+      <p><span>"{{ title }}" is the <i>interpolated</i> title.</span></p>
+      <p>"<span [innerHTML]="title"></span>" is the <i>property bound</i> title.</p>
+
+      evilTitle = 'Template <script>alert("evil never sleeps")</script>Syntax';
+
+      <p><span>"{{ evilTitle }}" is the <i>interpolated</i> evil title.</span></p>
+      <p><span [innerHTML]="evilTitle"></span> is the <i>property bound</i> evil title.</p>
   `
 };
 
-p6 = {
+attributeBinding = {
+  name: 'Attribute Binding',
+  code: `
+        <table border=1>
+                  <tr><td [attr.colspan]="1 + 1">One-Two</td></tr>
+                  <tr><td>Three</td><td>Four</td></tr>
+        </table>
+        <button [attr.aria-label]="actionName">{{ actionName }} with Aria</button>
+  `
+};
+
+classBinding = {
+  name: 'Class Binding',
+  code: `
+        <div class="nice curly special" [class]="niceCurly">Nice curly</div>
+        <div [class.special]="isSpecial">This class binding is special</div>
+        <div class="special" [class.special]="!isSpecial">This one is not so special</div>
+  `
+};
+
+styleBinding = {
+  name: 'Style Binding',
+  code: `
+        <button [style.color]="isSpecial ? 'red' : 'green'">Red</button>
+        <button [style.background-color]="canSave ? 'yellow' : 'blue'">Save Her</button>
+        <button [style.font-size.em]="isSpecial ? 3 : 1">Big</button>
+        <button [style.font-size.%]="!isSpecial ? 150 : 50">Small</button>
+  `
+};
+
+eventBinding = {
+  name: 'Event Binding',
+  code: `
+      <button (click)="onSave()">Save Her</button>
+      <button on-click="onSave()">Save Her</button>
+      <div (myClick)="clickMessage=$event" clickable>Click with myClick</div>
+      {{ clickMessage }}
+
+      <input [value]="currentHero.name" (input)="currentHero.name=$event.target.value">
+  `
+};
+
+customEvents = {
+  name: 'Custom Events with EventEmitter',
+  code: `
+    template: \`
+          <div>
+            <img src="{{ heroImageUrl }}">
+            <span [style.text-decoration]="lineThrough">
+                {{ prefix }} {{ hero?.name }}
+            </span>
+            <button (click)="delete()">Delete</button>
+          </div>
+    \`
+    deleteRequest = new EventEmitter<Hero>();
+    delete() {
+      this.deleteRequest.emit(this.hero);
+    }
+
+    <app-hero-detail (deleteRequest)="deleteHero($event)" [hero]="currentHero">
+    </app-hero-detail>
+  `
+};
+
+twoWayBinding = {
+  name: 'Two Way Binding',
+  code: `
+      import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+      @Component({
+        selector: 'app-sizer',
+        template: \`
+            <div>
+                <button (click)="decrease()" title="smaller">-</button>
+                <button (click)="increase()" title="bigger">+</button>
+                <label [style.font-size.px]="size">FontSize: {{ size }}px</label>
+            </div>
+        \`
+      })
+      export class SizerComponent {
+        @Input() size: number | string;
+        @Output() sizeChange = new EventEmitter<number>();
+
+        decrease() {
+          this.resize(-1);
+        }
+
+        increase() {
+          this.resize(+1);
+        }
+
+
+        resize(delta: number) {
+          this.size = Math.min(40, Math.max(8, +this.size + delta));
+          this.sizeChange.emit(this.size);
+        }
+
+      }
+
+      <app-sizer [(size)]="fontSizePx"></app-sizer>
+      <div [style.font-size.px]="fontSizePx">Resizable Text</div>
+
+      // desugared
+      <app-sizer [size]="fontSizePx" (sizeChange)="fontSizePx=$event"></app-sizer>
+
+  `
+};
+
+ngClass = {
+  name: 'NgClass',
+  code: `
+        <div [class.special]="isSpecial">This class binding is special.</div>
+
+        <div [ngClass]="currentClasses">
+        This div is initially saveable, unchanged, and special.
+        </div>
+
+        currentClasses: {};
+        setCurrentClasses() {
+          this.currentClasses = {
+            'saveable': this.canSave,
+            'modified': !this.isUnchanged,
+            'special': this.isSpecial
+          };
+        }
+  `
+};
+
+ngStyle = {
+  name: 'NgStyle',
+  code: `
+      <div [style.font-size]="isSpecial ? 'x-large' : 'smaller'">
+        This div is x-large or smaller.
+      </div>
+      <div [ngStyle]="currentStyles">
+        This div is initially italic, normal weight, and extra large (24px).
+      </div>
+      currentStyles: {};
+      setCurrentStyles() {
+        this.currentStyles = {
+          'font-style': this.canSave ? 'italic' : 'normal',
+          'font-weight': !this.isUnchanged ? 'bold' : 'normal',
+          'font-size': this.isSpecial ? '24px' : '12px'
+        };
+      }
+
+  `
+};
+
+ngModel = {
+  name: 'NgModel',
+  code: `
+      import { FormsModule } from '@angular/forms';
+
+      <input [(ngModel)]="currentHero.name">
+
+      <input [value]="currentHero.name" (input)="currentHero.name=$event.target.value">
+
+      <input [ngModel]="currentHero.name" (ngModelChange)="currentHero.name=$event">
+
+      <input [ngModel]="currentHero.name" (ngModelChange)="setUppercaseName($event)">
+  `
+};
+
+ngIfStructuralDirective = {
+  name: 'NgIf',
+  code: `
+      <app-hero-detail *ngIf="isActive"></app-hero-detail>
+
+      <div [class.hidden]="!isSpecial">Show with class</div>
+      <div [class.hidden]="isSpecial">Hide with class</div>
+      <app-hero-detail [class.hidden]="isSpecial"></app-hero-detail>
+
+      <div [style.display]="isSpecial ? 'block' : 'none'">Show with style</div>
+      <div [style.display]="isSpecial ? 'none' : 'block'">Hide with style</div>
+
+      <div *ngIf="currentHero">Hello, {{ currentHero.name }}</div>
+      <div *ngIf="nullHero">Hello, {{ nullHero.name }}</div>
+  `
+};
+
+ngForOf = {
+  name: 'NgForOf',
+  code: `
+        <div *ngFor="let hero of heroes">{{ hero.name }}</div>
+        <app-hero-detail *ngFor="let hero of heroes" [hero]="hero"></app-hero-detail>
+
+        <div *ngFor="let hero of heroes; let i = index;">{{ i + 1 }} - {{ hero.name }}</div>
+
+        <div *ngFor="let hero of heroes; trackBy: trackByHeroes">
+            ({{ hero.id }}) {{ hero.name }}
+        </div>
+
+        trackByHeroes(index: number, hero: Hero) {
+          return hero.id;
+        }
+  `
+};
+
+ngSwitch = {
+  name: 'NgSwitch',
+  code: `
+        <div [ngSwitch]="currentHero.emotion">
+              <app-happy-hero *ngSwitchCase="'happy'" [hero]="currentHero">
+              </app-happy-hero>
+              <app-sad-hero *ngSwitchCase="'sad'" [hero]="currentHero">
+              </app-sad-hero>
+              <app-confused-hero *ngSwitchCase="'confused'" [hero]="currentHero">
+              </app-confused-hero>
+              <div *ngSwitchCase="'confused'">
+              Are you as confused as {{ currentHero.name }}
+              </div>
+              <app-unknown-hero *ngSwitchDefault [hero]="currentHero">
+              </app-unknown-hero>
+        </div>
+  `
+};
+
+templateReferenceVariable = {
+  name: ' Template Reference Variable (#var)',
+  code: `
+        <input #phone placeholder="phone number">
+        <button (click)="callPhone(phone.value)">Call</button>
+
+        <form (ngSubmit)="onSubmit(heroForm)" #heroForm="ngForm">
+            <div class="form-group">
+                <label for="name">Name
+                    <input class="form-control" name="name" required [(ngModel)]="hero.name">
+                </label>
+            </div>
+            <button type="submit" [disabled]="!heroForm.form.valid">Submit</button>
+        </form>
+        <div [hidden]="!heroForm.form.valid">
+            {{ submitMessage }}
+        </div>
+
+        <input ref-fax placeholder="fax number">
+        <button (click)="callFax(fax.value)">Fax</button>
+  `
+};
+
+inputOutputProperties = {
+  name: 'Input && Output Properties',
+  code: `
+        <img [src]="iconUrl">
+        <button (click)="onSave()">Save</button>
+
+        <app-hero-detail [hero]="currentHero" (deleteRequest)="deleteHero($event)">
+        </app-hero-detail>
+
+        @Input() hero: Hero;
+        @Output() deleteRequest = new EventEmitter<Hero>();
+
+        @Component({
+          inputs: ['hero'],
+          outputs: ['deleteRequest']
+        })
+
+        <div (myClick)="clickMessage=$event" clickable>click with myClick</div>
+        @Output('myClick') clicks = new EventEmitter<string>();
+
+        @Directive({
+          outputs: ['clicks:myClick']
+        })
+
+  `
+};
+
+templateExpressionOperators = {
+  name: 'Template Expression Operators',
+  code: `
+      <div>Title through uppercase pipe: {{ title | uppercase }}</div>
+      <div>
+            Title through a pipe chain:
+            {{ title | uppercase | lowercase }}
+      </div>
+      <div>
+            Birthdate: {{ currentHero?.birthDate | date:'longDate' }}
+      </div>
+      <div> {{ currentHero | json }}</div>
+
+      <div>The current hero's name is {{ currentHero?.name }}</div>
+
+      <div *ngIf="nullHero">The null hero's name is {{ nullHero.name }}</div>
+      <div>The null hero's name is {{ nullHero && nullHero.name }}</div>
+      <div>The null hero's name is {{ nullHero?.name }}</div>
+
+      <div *ngIf="hero">
+            The hero's name is {{ hero!.name }}
+      </div>
+
+      <div>
+            The hero's marker is {{ $any(hero).marker }}
+      </div>
+
+      <div>
+            Undeclared members is {{ $any(this).member }}
+      </div>
+  `
+};
+
+p21 = {
   name: '',
   code: ``
 };
 
-p7 = {
+p22 = {
   name: '',
   code: ``
 };
 
-p8 = {
+p23 = {
   name: '',
   code: ``
 };
 
-p9 = {
+p24 = {
   name: '',
   code: ``
 };
 
-p10 = {
+p25 = {
   name: '',
   code: ``
 };
 
-p11 = {
+p26 = {
   name: '',
   code: ``
 };
 
-p12 = {
+p27 = {
+  name: '',
+  code: ``
+};
+
+p28 = {
+  name: '',
+  code: ``
+};
+
+p29 = {
+  name: '',
+  code: ``
+};
+
+p30 = {
+  name: '',
+  code: ``
+};
+
+p31 = {
+  name: '',
+  code: ``
+};
+
+p32 = {
+  name: '',
+  code: ``
+};
+
+p33 = {
+  name: '',
+  code: ``
+};
+
+p34 = {
+  name: '',
+  code: ``
+};
+
+p35 = {
+  name: '',
+  code: ``
+};
+
+p36 = {
+  name: '',
+  code: ``
+};
+
+p37 = {
+  name: '',
+  code: ``
+};
+
+p38 = {
+  name: '',
+  code: ``
+};
+
+p39 = {
+  name: '',
+  code: ``
+};
+
+p40 = {
+  name: '',
+  code: ``
+};
+
+p41 = {
+  name: '',
+  code: ``
+};
+
+p42 = {
   name: '',
   code: ``
 };
